647ae9f556d974c221b0834721e745d4a335a1d2
Maximbeshliu <maximbeshliu@yahoo.com>
2020-06-23 14:35:42 +0300

    git commit

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,4 +1,6 @@
 import { RenderPage } from './render.js';
 import '../styles/style.scss';
+import '../images/img.jpg';
+
 
 window.create = new RenderPage();
--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,15 @@ module.exports = {
                 ],
 
             },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            }
+
 
         ]
 
