5c5523ee96897b95d6abbfa6e19a89e58f22756d
Maximbeshliu <maximbeshliu@yahoo.com>
2020-07-03 23:31:38 +0300

    git commit

--- src/JS/shared.js
+++ src/JS/shared.js
@@ -21,6 +21,32 @@ function changePage() {
 
 changePage();
 
+function burgerMenu() {
+    let burger = document.querySelector('.burger-menu');
+    let btnMenu = document.querySelector('.burger-menu__button');
+    let menuItem = document.querySelector('.menu');
+    let overlay = document.querySelector('.burger-menu__overlay');
+
+    btnMenu.addEventListener('click', (event) => {
+        if (event.target) {
+            burger.classList.toggle('burger-menu__active');
+        }
+    });
+    overlay.addEventListener('click', (event) => {
+        if (event.target) {
+            burger.classList.remove('burger-menu__active');
+        }
+    });
+
+    menuItem.addEventListener('click', (event) => {
+        if (event.target) {
+            burger.classList.remove('burger-menu__active');
+        }
+    });
+}
+
+burgerMenu();
+
 
 window.create = new RederHeader;
 
